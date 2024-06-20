fb13f61768a7a19aef1c6949034c97caed079d32
c-campbell4 <143621339+c-campbell4@users.noreply.github.com>
2024-06-20 10:16:36 -0500

    Add files via upload

---
+++ green_myLogonScript.ps1
@@ -0,0 +1,81 @@
+﻿
+
+
+####### Part 1 #######
+## Set to Dark Mode
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+
+
+
+
+
+####### Part 2 #######
+## Set Accent Color
+$RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent"
+
+#Accent Color Menu Key
+$AccentColorMenuKey = @{
+	Key   = 'AccentColorMenu';
+	Type  = "DWORD";
+	Value = '0xff6acc00'
+}
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $AccentColorMenuKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $AccentColorMenuKey.Key -Value $AccentColorMenuKey.Value -PropertyType $AccentColorMenuKey.Type -Force
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $AccentColorMenuKey.Key -Value $AccentColorMenuKey.Value -Force
+}
+
+#Accent Palette Key
+$AccentPaletteKey = @{
+	Key   = 'AccentPalette';
+	Type  = "BINARY";
+	Value = 'c1,f7,dd,00,a6,f7,d0,00,68,e3,a8,00,00,cc,6a,00,00,87,46,00,00,52,2a,00,00,2b,16,00,e3,00,8c,00'
+}
+$hexified = $AccentPaletteKey.Value.Split(',') | ForEach-Object { "0x$_" }
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $AccentPaletteKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $AccentPaletteKey.Key -PropertyType Binary -Value ([byte[]]$hexified)
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $AccentPaletteKey.Key -Value ([byte[]]$hexified) -Force
+}
+
+#MotionAccentId_v1.00 Key
+$MotionAccentIdKey = @{
+	Key   = 'MotionAccentId_v1.00';
+	Type  = "DWORD";
+	Value = '0x000000db'
+}
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $MotionAccentIdKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $MotionAccentIdKey.Key -Value $MotionAccentIdKey.Value -PropertyType $MotionAccentIdKey.Type -Force
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $MotionAccentIdKey.Key -Value $MotionAccentIdKey.Value -Force
+}
+
+#Start Color Menu Key
+$StartMenuKey = @{
+	Key   = 'StartColorMenu';
+	Type  = "DWORD";
+	Value = '0xff468700'
+}
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $StartMenuKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $StartMenuKey.Key -Value $StartMenuKey.Value -PropertyType $StartMenuKey.Type -Force
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $StartMenuKey.Key -Value $StartMenuKey.Value -Force
+}
+
+Stop-Process -ProcessName explorer -Force -ErrorAction SilentlyContinue
---
+++ red_myLogonScript.ps1
@@ -0,0 +1,82 @@
+﻿
+
+
+####### Part 1 #######
+## Set to Dark Mode
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+
+
+
+
+
+####### Part 2 #######
+## Set Accent Color
+$RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent"
+
+#Accent Color Menu Key
+$AccentColorMenuKey = @{
+	Key   = 'AccentColorMenu';
+	Type  = "DWORD";
+	Value = '0xff2311e8'
+}
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $AccentColorMenuKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $AccentColorMenuKey.Key -Value $AccentColorMenuKey.Value -PropertyType $AccentColorMenuKey.Type -Force
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $AccentColorMenuKey.Key -Value $AccentColorMenuKey.Value -Force
+}
+
+#Accent Palette Key
+$AccentPaletteKey = @{
+	Key   = 'AccentPalette';
+	Type  = "BINARY";
+	Value = 'ff,bd,c2,00,ff,99,a1,00,f0,59,65,00,e8,11,23,00,99,00,0d,00,6e,00,09,00,47,00,06,00,69,79,7e,00'
+}
+$hexified = $AccentPaletteKey.Value.Split(',') | ForEach-Object { "0x$_" }
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $AccentPaletteKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $AccentPaletteKey.Key -PropertyType Binary -Value ([byte[]]$hexified)
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $AccentPaletteKey.Key -Value ([byte[]]$hexified) -Force
+}
+
+#MotionAccentId_v1.00 Key
+$MotionAccentIdKey = @{
+	Key   = 'MotionAccentId_v1.00';
+	Type  = "DWORD";
+	Value = '0x000000db'
+}
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $MotionAccentIdKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $MotionAccentIdKey.Key -Value $MotionAccentIdKey.Value -PropertyType $MotionAccentIdKey.Type -Force
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $MotionAccentIdKey.Key -Value $MotionAccentIdKey.Value -Force
+}
+
+#Start Color Menu Key
+$StartMenuKey = @{
+	Key   = 'StartColorMenu';
+	Type  = "DWORD";
+	Value = '0xff0d0099'
+}
+
+If ($Null -eq (Get-ItemProperty -Path $RegPath -Name $StartMenuKey.Key -ErrorAction SilentlyContinue))
+{
+	New-ItemProperty -Path $RegPath -Name $StartMenuKey.Key -Value $StartMenuKey.Value -PropertyType $StartMenuKey.Type -Force
+}
+Else
+{
+	Set-ItemProperty -Path $RegPath -Name $StartMenuKey.Key -Value $StartMenuKey.Value -Force
+}
+
+
+Stop-Process -ProcessName explorer -Force -ErrorAction SilentlyContinue
